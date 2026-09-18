42caf0243eb0baa859017bd8c2c1e30519f941fc
Paul Bullock <pkbullock@users.noreply.github.com>
2026-09-18 22:16:21 +0100

    Visual upgrades, placeholders

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://pkbullock.github.io/modernization-kit',
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   }
